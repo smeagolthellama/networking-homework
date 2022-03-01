53e2b5ad346a3b557e262667b3843d2dfd187503
kocka_collector <babiak@disroot.org>
2022-03-01 17:37:07 +0200

    finish client program

--- lab1/client.pl
+++ lab1/client.pl
@@ -11,5 +11,21 @@ setsockopt($socket, SOL_SOCKET, SO_REUSEADDR, 1)
 
 my $sockaddr=pack_sockaddr_in(10001,inet_aton("127.0.0.1"));
 
+connect($socket, $sockaddr)
+	or die("connect:$!");
 
+$socket->autoflush(1);
 
+while(my $line=<>){
+	print $socket $line
+		or last;
+	$line=<$socket>;
+		
+	if(defined $line){
+		print $line;
+	}else{
+		last;
+	}
+}
+
+close $socket;
