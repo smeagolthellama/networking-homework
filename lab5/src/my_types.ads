e798e8bc1dd1bc86e71158e320f4f3a5455c3bcc
kocka_collector <babiak@disroot.org>
2022-05-09 14:40:06 +0300

    pack arrays

--- lab5/src/my_types.ads
+++ lab5/src/my_types.ads
@@ -4,8 +4,8 @@ with Ada.Text_IO.Complex_IO;
 package My_Types is
    type My_Range is range 1 .. 720;
    type My_Float is digits 5;
-   type My_Arr is array (My_Range) of Boolean;
-   type My_Mat is array (My_Range,My_Range) of Boolean;
+   type My_Arr is array (My_Range) of Boolean with Pack;
+   type My_Mat is array (My_Range,My_Range) of Boolean with Pack;
    package m_c is new Ada.Numerics.Generic_Complex_Types(Real => My_Float);
    package c_io is new Ada.Text_IO.Complex_IO(m_c);
 end My_Types;
