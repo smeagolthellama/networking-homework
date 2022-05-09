db086f1feb1327b12649898d561cee84b0ab3257
kocka_collector <babiak@disroot.org>
2022-05-09 10:36:44 +0300

    fix complex type and add array range

--- lab5/src/my_types.ads
+++ lab5/src/my_types.ads
@@ -1,7 +1,7 @@
 with Ada.Numerics.Generic_Complex_Types;
 
 package My_Types is
+   type My_Range is range 1 .. 720;
    type My_Float is digits 5;
-   package m_c is new Generic_Complex_Types(Real => My_Float);
-   use m_c;
+   package m_c is new Ada.Numerics.Generic_Complex_Types(Real => My_Float);
 end My_Types;
