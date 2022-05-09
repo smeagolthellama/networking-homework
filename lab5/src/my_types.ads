19e94492ea10691fdbb5a88127412bda4793e324
kocka_collector <babiak@disroot.org>
2022-05-09 11:09:31 +0300

    add array type

--- lab5/src/my_types.ads
+++ lab5/src/my_types.ads
@@ -3,5 +3,6 @@ with Ada.Numerics.Generic_Complex_Types;
 package My_Types is
    type My_Range is range 1 .. 720;
    type My_Float is digits 5;
+   type My_Arr is array (My_Range) of Boolean;
    package m_c is new Ada.Numerics.Generic_Complex_Types(Real => My_Float);
 end My_Types;
