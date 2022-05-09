4ddb76ea08de4bd666596cc9b5531a61e5c58398
kocka_collector <babiak@disroot.org>
2022-05-09 14:03:31 +0300

    fix constraint problems in a bad way

--- lab5/src/client.adb
+++ lab5/src/client.adb
@@ -1,7 +1,7 @@
 with GNAT.Sockets; use GNAT.Sockets;
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Command_Line; use Ada.Command_Line;
-with My_Types; use My_Types; use My_Types.m_c;
+with My_Types; use My_Types; use My_Types.m_c; use My_Types.c_io;
 
 procedure client is
    sock: Socket_Type;
@@ -28,33 +28,41 @@ begin
       end_cplx: constant Complex:=Complex'Input(data_stream);
       index_cplx: Complex:=(-2.0,1.2);
       diff_r_cplx: constant Complex:=(4.0/720.0,0.0);
-      diff_i_cplx: constant Complex:=(0.0,2.4/720.0);
-      count: My_Range:=1;
-      row_index: My_Range;
+      diff_i_cplx: constant Complex:=(0.0,-2.4/720.0);
+      count: Integer:=1;
+      row_index: Integer;
       arr: My_Arr;
       iter: Integer:=1;
       z: Complex;
    begin
+      Put_Line(Standard_Error, "got start position: ");
+      Put(Standard_Error,start_cplx);
+      Put_Line(Standard_Error,"");
+      Put_Line(Standard_Error, "got end position: ");
+      Put(Standard_Error,end_cplx);
+      Put_Line(Standard_Error,"");
       -- find the points within the rectangle given.
       while index_cplx.Im>start_cplx.Im loop
          index_cplx:=index_cplx+diff_i_cplx;
          count:=count+1;
       end loop;
-
-      while index_cplx.Im<=end_cplx.Im loop
+      Put_Line("starting at line "&count'Image);
+      while index_cplx.Im+diff_i_cplx.Im>=end_cplx.Im loop
          row_index:=1;
-         while index_cplx.Re<=end_cplx.Re loop
+         while index_cplx.Re<end_cplx.Re and row_index<=720 loop
             iter:=1;
             z:=index_cplx;
             while iter<Integer(My_Range'Last) and Modulus(z)<=2.0 loop
                z:=z**2+index_cplx;
                iter:=iter+1;
             end loop;
-            arr(row_index):=(Modulus(z)<=2.0);
+            arr(My_Range(row_index)):=(Modulus(z)<=2.0);
             index_cplx:=index_cplx+diff_r_cplx;
+            --Put_Line("row_index is now "&row_index'Image);
             row_index:=row_index+1;
          end loop;
-         My_Range'Output(data_stream,count);
+         Put_Line("Calculated line "&count'Image&". Sending it to the socket.");
+         My_Range'Output(data_stream,My_Range(count));
          My_Arr'Output(data_stream,arr);
          count:=count+1;
          index_cplx:=index_cplx+diff_i_cplx;
