305c183b067a6bd4738babf77d9aac30e20903c2
kocka_collector <babiak@disroot.org>
2022-05-09 14:04:16 +0300

    add io for complex number type

--- lab5/src/my_types.ads
+++ lab5/src/my_types.ads
@@ -1,4 +1,5 @@
 with Ada.Numerics.Generic_Complex_Types;
+with Ada.Text_IO.Complex_IO;
 
 package My_Types is
    type My_Range is range 1 .. 720;
@@ -6,4 +7,5 @@ package My_Types is
    type My_Arr is array (My_Range) of Boolean;
    type My_Mat is array (My_Range,My_Range) of Boolean;
    package m_c is new Ada.Numerics.Generic_Complex_Types(Real => My_Float);
+   package c_io is new Ada.Text_IO.Complex_IO(m_c);
 end My_Types;
