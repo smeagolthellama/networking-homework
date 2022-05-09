4bd662be4a69f1fdc667a41cb029d38e6ccc7cd8
kocka_collector <babiak@disroot.org>
2022-05-09 11:47:26 +0300

    add matrix type

--- lab5/src/my_types.ads
+++ lab5/src/my_types.ads
@@ -4,5 +4,6 @@ package My_Types is
    type My_Range is range 1 .. 720;
    type My_Float is digits 5;
    type My_Arr is array (My_Range) of Boolean;
+   type My_Mat is array (My_Range,My_Range) of Boolean;
    package m_c is new Ada.Numerics.Generic_Complex_Types(Real => My_Float);
 end My_Types;
